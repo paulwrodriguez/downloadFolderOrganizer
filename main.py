f304b585cd98ceef3393f327f76e83ee1a86ef25
Paul <linkmasterx23@gmail.com>
2019-12-23 17:01:20 -0700

    fixed issue with it not working

--- main.py
+++ main.py
@@ -6,33 +6,41 @@ from watchdog.events import PatternMatchingEventHandler
 def handle_event(event):
     print("inside the handle event")
 
+def on_created(event):
+    print(f"hey, {event.src_path} has been created!")
+
+def on_deleted(event):
+    print(f"what the f**k! Someone deleted {event.src_path}!")
+
+def on_modified(event):
+    print(f"hey buddy, {event.src_path} has been modified")
+
+def on_moved(event):
+    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+
 
 if __name__ == "__main__":
     print("inside main")
-    pattern = "*"
-    ignorePatter = ""
-    ignoreDirectory = False
+    patterns = "*"
+    ignore_patterns = ""
+    ignore_directories = False
     case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(pattern,ignorePatter, ignoreDirectory, case_sensitive)
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-    my_event_handler.on_any_event(handle_event)
-    my_event_handler.on_created(handle_event)
-    my_event_handler.on_modified(handle_event)
-    my_event_handler.on_moved(handle_event)
+    my_event_handler.on_created = on_created
+    my_event_handler.on_deleted = on_deleted
+    my_event_handler.on_modified = on_modified
+    my_event_handler.on_moved = on_moved
 
-    path = "."
-    go_recursive = True
+    path = "C:\\Users\\Paul\\Desktop\\Cerebro\python"
+    go_recursively = True
     my_observer = Observer()
-    my_observer.schedule(my_event_handler, path, recursive=go_recursive)
+    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
 
     my_observer.start()
     try:
         while True:
-            time.sleep(5)
+            time.sleep(1)
     except KeyboardInterrupt:
-        print("inside the except")
         my_observer.stop()
         my_observer.join()
-
-
-
