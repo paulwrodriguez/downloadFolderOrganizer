481e7ceb6171d673a4bb6c668d2e5cb52c2731f7
Paul <linkmasterx23@gmail.com>
2019-12-26 18:22:01 -0700

    moved file processing to method

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import time
 import ntpath
 import threading
 from pathlib import Path
-from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
 
 src = "C:\\Users\\Paul\\Desktop\\Cerebro\\temp\\src\\downloads"
 des = "C:\\Users\\Paul\\Desktop\\Cerebro\\temp\\dest"
@@ -40,24 +38,6 @@ def rename_file(src_path, des_path):
     rename_file_helper(src_path, des_path, tries, max_tries, sleep_time)
 
 
-def on_modified(event):
-    print("event:", event.event_type, " src:", event.src_path)
-    basename = ntpath.basename(event.src_path)
-
-    if event.src_path.endswith('.txt'):
-        new_name = des + '''\\txt\\''' + basename
-    elif event.src_path.endswith('.exe'):
-        new_name = des + '''\\exe\\''' + basename
-    elif event.src_path.endswith('.pdf'):
-        new_name = des + '''\\pdf\\''' + basename
-    elif event.src_path.endswith('.zip'):
-        new_name = des + '''\\zip\\''' + basename
-    else:
-        return
-
-    rename_file(event.src_path, new_name)
-
-
 def mkdirs():
     os.makedirs(des + "\\txt", exist_ok=True)
     os.makedirs(des + "\\exe", exist_ok=True)
@@ -75,56 +55,33 @@ def create_test_files(src):
     Path(src + '''\\exe.exe''').touch()
     Path(src + '''\\zip.zip''').touch()
 
+def process_file(filename):
+    basename = ntpath.basename(filename)
+
+    if filename.endswith('.txt'):
+        new_name = des + '''\\txt\\''' + basename
+    elif filename.endswith('.exe'):
+        new_name = des + '''\\exe\\''' + basename
+    elif filename.endswith('.pdf'):
+        new_name = des + '''\\pdf\\''' + basename
+    elif filename.endswith('.zip'):
+        new_name = des + '''\\zip\\''' + basename
+    else:
+        return
+
+    rename_file(src + '''\\''' + filename, new_name)
+
 
 if __name__ == "__main__":
     mkdirs()  # create directories that will be supported
+    create_test_files(src)  # Make test files
+
     if len(os.listdir()) <= 0:
         print("Error: Could not find any files inside ", src, " directory")
 
-    print("Changing to ", src, "directory")
     os.chdir(src)
-    create_test_files(src)
 
-    # cycle through each file in the folder
     for filename in os.listdir():
-        # send file to appropriate sub folder
-        basename = ntpath.basename(filename)
-
-        if filename.endswith('.txt'):
-            new_name = des + '''\\txt\\''' + basename
-        elif filename.endswith('.exe'):
-            new_name = des + '''\\exe\\''' + basename
-        elif filename.endswith('.pdf'):
-            new_name = des + '''\\pdf\\''' + basename
-        elif filename.endswith('.zip'):
-            new_name = des + '''\\zip\\''' + basename
-        else:
-            continue
-
-        rename_file(src + '''\\''' + filename, new_name)
+        process_file(filename)  # send file to appropriate sub folder
 
 
-
-    # patterns = "*"
-    # ignore_patterns = ""
-    # ignore_directories = False
-    # case_sensitive = True
-    # my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
-
-    # my_event_handler.on_created = on_modified
-    # my_event_handler.on_deleted = on_deleted
-    # my_event_handler.on_modified = on_modified
-    # my_event_handler.on_moved = on_moved
-
-    # path = src
-    # go_recursively = True
-    # my_observer = Observer()
-    # my_observer.schedule(my_event_handler, path, go_recursively)
-
-    # my_observer.start()
-    # try:
-    #     while True:
-    #         time.sleep(5)
-    # except KeyboardInterrupt:
-    #     my_observer.stop()
-    #     my_observer.join()
